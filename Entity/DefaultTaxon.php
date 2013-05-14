b97ab385319d47bf8a1fff6d5bea2552542dfa1c
Marcin Dryka <marcin@dryka.pl>
2013-05-14 14:58:17 +0200

    Moving definition

--- Entity/DefaultTaxon.php
+++ Entity/DefaultTaxon.php
@@ -18,4 +18,54 @@ namespace Sylius\Bundle\TaxonomiesBundle\Entity;
  */
 class DefaultTaxon extends Taxon
 {
+    /**
+     * Required by DoctrineExtensions.
+     *
+     * @var mixed
+     */
+    protected $left;
+
+    /**
+     * Required by DoctrineExtensions.
+     *
+     * @var mixed
+     */
+    protected $right;
+
+    /**
+     * Required by DoctrineExtensions.
+     *
+     * @var mixed
+     */
+    protected $level;
+
+    public function getLeft()
+    {
+        return $this->left;
+    }
+
+    public function setLeft($left)
+    {
+        $this->left = $left;
+    }
+
+    public function getRight()
+    {
+        return $this->right;
+    }
+
+    public function setRight($right)
+    {
+        $this->right = $right;
+    }
+
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    public function setLevel($level)
+    {
+        $this->level = $level;
+    }
 }
